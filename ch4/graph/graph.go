b6c74e0ed9542922fecffd3bf34c9eac98715c69
sfw <909075325@qq.com>
2018-05-20 18:15:19 +0800

    --add graph--

--- ch4/graph/graph.go
+++ ch4/graph/graph.go
@@ -1,31 +1,23 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 99.
-
-// Graph shows how to use a map of maps to represent a directed graph.
 package main
 
 import "fmt"
 
-//!+
-var graph = make(map[string]map[string]bool)
+var graph = make(map[string] map[string]bool)
 
-func addEdge(from, to string) {
+func addEdge(from, to string){
 	edges := graph[from]
-	if edges == nil {
+
+	//k-v 不存在，创建一个
+	if edges == nil{
 		edges = make(map[string]bool)
 		graph[from] = edges
 	}
 	edges[to] = true
 }
 
-func hasEdge(from, to string) bool {
+func hasEdge(from, to string)bool{
 	return graph[from][to]
 }
-
-//!-
-
 func main() {
 	addEdge("a", "b")
 	addEdge("c", "d")
@@ -39,5 +31,4 @@ func main() {
 	fmt.Println(hasEdge("c", "d"))
 	fmt.Println(hasEdge("x", "d"))
 	fmt.Println(hasEdge("d", "x"))
-
 }
