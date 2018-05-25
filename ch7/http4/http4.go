65de2778d4c42abac8cd73d54cbc8db5102dde1c
sfw <909075325@qq.com>
2018-05-25 21:25:20 +0800

    --add http4--

--- ch7/http4/http4.go
+++ ch7/http4/http4.go
@@ -1,19 +1,27 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
-// See page 195.
-
-// Http4 is an e-commerce server that registers the /list and /price
-// endpoint by calling http.HandleFunc.
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
+	"log"
+	"fmt"
 )
 
-//!+main
+/*
+// Serve a new connection.
+func (c *conn) 66(ctx context.Context) {
+				h := initNPNRequest{tlsConn, serverHandler{c.server}}
+		serverHandler{c.server}.ServeHTTP(w, w.req)
+}
+
+func (sh serverHandler) ServeHTTP(rw ResponseWriter, req *Request) {
+		handler = DefaultServeMux
+}
+*/
+
+//所以为了方便，net/http包提供了一个全局的ServeMux实例DefaultServerMux和包级别的http.Handle和http.HandleFunc函数。
+//现在，为了使用DefaultServeMux作为服务器的主handler，我们不需要将它传给ListenAndServe函数；nil值就可以工作。
+//然后服务器的主函数可以简化成：
 
 func main() {
 	db := database{"shoes": 50, "socks": 5}
@@ -21,8 +29,6 @@ func main() {
 	http.HandleFunc("/price", db.price)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
-
-//!-main
 
 type dollars float32
 
