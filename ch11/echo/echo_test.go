36affd159c5719016f4ef186e28d7f9c94aff3c6
sfw <909075325@qq.com>
2018-06-03 17:27:50 +0800

    --add echo--

--- ch11/echo/echo_test.go
+++ ch11/echo/echo_test.go
@@ -1,45 +1,47 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// Test of echo command.  Run with: go test gopl.io/ch11/echo
-
-//!+
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"testing"
+	"fmt"
+	"bytes"
 )
 
-func TestEcho(t *testing.T) {
-	var tests = []struct {
+func TestEcho(t *testing.T){
+	var tests = []struct{
 		newline bool
-		sep     string
-		args    []string
-		want    string
+		sep 	string
+		args 	[]string
+		want 	string
 	}{
-		{true, "", []string{}, "\n"},
+		{true,"",[]string{},"\n"},
 		{false, "", []string{}, ""},
 		{true, "\t", []string{"one", "two", "three"}, "one\ttwo\tthree\n"},
 		{true, ",", []string{"a", "b", "c"}, "a,b,c\n"},
 		{false, ":", []string{"1", "2", "3"}, "1:2:3"},
 	}
 
-	for _, test := range tests {
+	for _, test := range tests{
+
 		descr := fmt.Sprintf("echo(%v, %q, %q)",
 			test.newline, test.sep, test.args)
 
-		out = new(bytes.Buffer) // captured output
-		if err := echo(test.newline, test.sep, test.args); err != nil {
-			t.Errorf("%s failed: %v", descr, err)
+		out = new(bytes.Buffer)
+		if err := echo(test.newline, test.sep, test.args); err != nil{
+			//t.Errorf<-------------
+			t.Errorf("%s failed: %v",descr, err)
 			continue
 		}
-		got := out.(*bytes.Buffer).String()
-		if got != test.want {
-			t.Errorf("%s = %q, want %q", descr, got, test.want)
-		}
-	}
-}
 
-//!-
+		got := out.(*bytes.Buffer).String()
+		if got != test.want{
+			//t.Errorf<-------------
+			//描述，结果，期望
+			t.Errorf("--> %s = %q, want %q", descr, got, test.want)
+		}
+
+
+	}
+	//fmt.Printf("-->%T-%T\n", out, new(string))
+	//fmt.Printf("-->%T\n", out.(*bytes.Buffer))
+
+}
