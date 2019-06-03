c8dd3b371616e82b1e94e7d3e7b17116bc9b3f1c
wahaha <xxxfuwen@gmail.com>
2019-06-03 18:51:56 +0800

    Update defer2.go

--- ch5/defer2/defer2.go
+++ ch5/defer2/defer2.go
@@ -22,3 +22,41 @@ func f(x int) {
 	defer fmt.Printf("defer %d\n", x)
 	f(x - 1)
 }
+
+/*
+f(3)
+f(2)
+f(1)
+defer 1
+defer 2
+defer 3
+goroutine 1 [running]:
+main.printStack()
+        /mnt/i/github.com/gkt_cc_go/src/example/defer2/defer2.go:16 +0x5b
+panic(0x4a20c0, 0x554270)
+        /usr/local/go/src/runtime/panic.go:522 +0x1b5
+main.f(0x0)
+        /mnt/i/github.com/gkt_cc_go/src/example/defer2/defer2.go:21 +0x174
+main.f(0x1)
+        /mnt/i/github.com/gkt_cc_go/src/example/defer2/defer2.go:23 +0x14f
+main.f(0x2)
+        /mnt/i/github.com/gkt_cc_go/src/example/defer2/defer2.go:23 +0x14f
+main.f(0x3)
+        /mnt/i/github.com/gkt_cc_go/src/example/defer2/defer2.go:23 +0x14f
+main.main()
+        /mnt/i/github.com/gkt_cc_go/src/example/defer2/defer2.go:11 +0x46
+panic: runtime error: integer divide by zero
+
+goroutine 1 [running]:
+main.f(0x0)
+        /mnt/i/github.com/gkt_cc_go/src/example/defer2/defer2.go:21 +0x174
+main.f(0x1)
+        /mnt/i/github.com/gkt_cc_go/src/example/defer2/defer2.go:23 +0x14f
+main.f(0x2)
+        /mnt/i/github.com/gkt_cc_go/src/example/defer2/defer2.go:23 +0x14f
+main.f(0x3)
+        /mnt/i/github.com/gkt_cc_go/src/example/defer2/defer2.go:23 +0x14f
+main.main()
+        /mnt/i/github.com/gkt_cc_go/src/example/defer2/defer2.go:11 +0x46
+exit status 2
+*/
